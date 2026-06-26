2a6b19dec805da6327c93dd77e1171390645fa77
Darshana Selvakumar <darshanaselvakumar2007@gmail.com>
2026-06-26 22:07:46 +0530

    Organize backend API structure for deployment

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,8 +1,11 @@
-const { PrismaClient } = require("@prisma/client");
-const bcrypt = require("bcrypt");
+ const bcrypt = require("bcrypt");
+
 const jwt = require("jsonwebtoken");
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 
 // REGISTER USER
